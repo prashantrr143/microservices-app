5fe1c2b5fb11efed85e2c83ba25983b0650889ca
prasingh26 <prasingh26@WKWIN1194166.global.publicisgroupe.net>
2018-08-29 09:22:44 +0530

    Initial Commit : Project setup

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # microservices-app
-Microservices-Application
+
+A repository containing the components used in microservices architecture based application.
+
+The project in this repository is a multimodule maven project, which has below listed modules currently :
+
+1. eureka-server-spring
+2. sleuth-spring-poc
+3. zipkin-spring-poc
---
+++ sleuth-spring-poc/src/main/java/com/prashant/loggin/sleuth/SleuthSpringPocApplication.java
@@ -0,0 +1,12 @@
+package com.prashant.loggin.sleuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleuthSpringPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SleuthSpringPocApplication.class, args);
+	}
+}
